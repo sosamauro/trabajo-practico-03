eeb6f8c7503ce43c8f13a5b8afdc8995722fa3fd
Mauro Sosa <mauro4sosa@gmail.com>
2022-11-10 16:10:05 -0300

    Subo archivos de Ejercicio 4.

---
+++ Ejercicio04/Encriptador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04
+{
+    public abstract class Encriptador
+    {
+        private String iNombre;
+
+        public Encriptador(String pNombre)
+        {
+            this.iNombre = pNombre;
+        }
+
+        public String Nombre
+        {
+            get { return this.iNombre; }
+            set { this.iNombre = value; }
+        }
+
+        public String EncriptarCadena
+    }
+}
