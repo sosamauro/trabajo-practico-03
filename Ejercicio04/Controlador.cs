112d94958370760900a9ac1b33f510177e9248c5
Mauro Sosa <mauro4sosa@gmail.com>
2022-11-18 13:55:03 -0300

    Encriptador Cesar hecho.

---
+++ Ejercicio04/Controlador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04
+{
+    public static class Controlador
+    {
+        public static string EncriptarCadena(string pCadena, string pEncriptador)
+        {
+            FabricaEncriptadores instancia = FabricaEncriptadores.Instancia();
+            IEncriptador encriptador = instancia.GetEncriptador(pEncriptador);
+            string cadenaEncriptada = encriptador.Encriptar(pCadena);
+            return cadenaEncriptada;
+        }
+
+        public static string DesencriptarCadena(string pCadena, string pEncriptador)
+        {
+            FabricaEncriptadores instancia = FabricaEncriptadores.Instancia();
+            IEncriptador encriptador = instancia.GetEncriptador(pEncriptador);
+            string cadenaDesencriptada = encriptador.Desencriptar(pCadena);
+            return cadenaDesencriptada;
+        }
+    }
+}
--- Ejercicio04/Program.cs
+++ Ejercicio04/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace Ejercicio04
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Boolean salir = false;
+
+            while (salir == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Elija un encriptador: ");
+                Console.WriteLine();
+                Console.WriteLine("1) Cesar");
+                Console.WriteLine("2) Advanced Encryption Standard (AES)");
+                Console.WriteLine();
+                Console.WriteLine("3) Salir");
+                Console.WriteLine();
+                Console.Write("Opción: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.Write("Ingrese la cadena a encriptar: ");
+                        string cadena = Console.ReadLine();
+                        string cadenaEncriptada = Controlador.EncriptarCadena(cadena, "Cesar");
+                        string cadenaDesencriptada = Controlador.DesencriptarCadena(cadenaEncriptada, "Cesar");
+                        Console.WriteLine();
+                        Console.WriteLine($"La cadena encriptada es: {cadenaEncriptada}");
+                        Console.WriteLine();
+                        Console.WriteLine($"La cadena desencriptada es: {cadenaDesencriptada}");
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        Console.Clear();
+                        salir = true;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese una opción válida.");
+                        Console.WriteLine();
+                        Console.Write("Presione ENTER para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
         }
     }
 }
